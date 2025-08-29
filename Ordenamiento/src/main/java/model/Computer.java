66b2d511905e66b62f929ccfb5fcfe938c46b73c
Laura Andrea Cárdenas Zuleta <lauraacardenaszs40@gmail.com>
2025-08-28 21:22:56 -0500

    2323

---
+++ Ordenamiento/src/main/java/model/Computer.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Computer implements Comparable<Computer> {
+    private String brand;
+    private int ram;
+    private int processsors;
+    private double processingSpeed;
+    private char operatingSystem;
+
+    public Computer(String brand, int ram, int processsors, double processingSpeed, char operatingSystem) {
+        this.brand = brand;
+        this.ram = ram;
+        this.processsors = processsors;
+        this.processingSpeed = processingSpeed;
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getProcesssors() {
+        return processsors;
+    }
+
+    public void setProcesssors(int processsors) {
+        this.processsors = processsors;
+    }
+
+    public double getProcessingSpeed() {
+        return processingSpeed;
+    }
+
+    public void setProcessingSpeed(double processingSpeed) {
+        this.processingSpeed = processingSpeed;
+    }
+
+    public char getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(char operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    @Override
+    public int compareTo(Computer otherComputer) {
+      return Double.compare(this.processingSpeed, otherComputer.getProcessingSpeed());
+    }
+}
---
+++ Ordenamiento/src/main/java/ui/Main.java
@@ -0,0 +1,10 @@
+package ui;
+import model.ComputersSale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
